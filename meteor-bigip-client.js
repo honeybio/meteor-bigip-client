a0ba6a1e7ac20ff0e4392b231342b0210dc43c57
Annika Stahl <annika@bespintech.com>
2015-12-01 17:52:31 -0500

    initial testing

--- meteor-bigip-client.js
+++ meteor-bigip-client.js
@@ -1,1 +1,25 @@
-// Write your package code here!
+var iControlRest;
+
+iControlRest = Npm.require('request');
+
+var bigipRestGet = function(ip, user, pass, url) {
+  /**
+  * Method Description
+  *
+  * @method Method Name
+  * @param {object} Parameters
+  * @return {boolean} return value
+  */
+  var reqUrl = url.replace(/localhost/, ip);
+
+  iControlRest(reqUrl, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (res.statusCode == 200) {
+        console.log(body)
+        return body;
+      }
+    }
+  });
+}
